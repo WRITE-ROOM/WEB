71aa6469f332eda9225590d828e058ec02c0efa6
youngju6143 <youngju6143@naver.com>
2024-01-31 21:59:09 +0900

    #17feat: 로그인 UI, 소셜 로그인 구현

---
+++ writeroom/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginBox from '../components/Login/LoginBox/LoginBox'
+import Header from '../components/Header/Header'
+
+export default function Login() {
+  return (
+    <div>
+			<Header/>
+      <LoginBox/>
+    </div>
+  )
+}
