c1a5fd8be1bb0b3bad7ab1a9fa7eff31f437c041
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2024-02-04 03:14:19 +0900

    fix #41: 오류 해결

--- writeroom/src/components/RoomMain/RoomMain.jsx
+++ writeroom/src/components/RoomMain/RoomMain.jsx
@@ -1,6 +1,5 @@
 import RoomNoteBox from "../RoomNoteBox/RoomNoteBox";
 import * as S from "./RoomMain.style";
-<<<<<<< feature/#27
 import TagSearchBox from "../TagSearchBox/TagSearchBox";
 import ImageRoomNoteBox from "../ImageRoomNoteBox/ImageRoomNoteBox";
 import Pagination from "react-js-pagination";
@@ -37,21 +36,13 @@ const RoomMain = ({ openRoomSNB, openSNB }) => {
     <S.Container>
       <S.ImgContainer
         openRoomSNB={openRoomSNB}
-=======
-
-const RoomMain = ({ openRoomSDB, openSNB }) => {
-  return (
-    <S.Container>
-      <S.ImgContainer
-        openRoomSDB={openRoomSDB}
->>>>>>> main
         openSNB={openSNB}
         src="https://images.unsplash.com/photo-1682687220777-2c60708d6889?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         // 테스트 이미지 링크
       />
       <S.NoteList>
         <h2>처음부터 완벽하게 쓰려는 생각을 버리고 그냥 써라</h2>
-<<<<<<< feature/#27
+
         <S.TopBox openRoomSNB={openRoomSNB} openSNB={openSNB}>
           <h1>스포츠에 대한 고찰</h1>
           <TagSearchBox />
@@ -83,14 +74,6 @@ const RoomMain = ({ openRoomSDB, openSNB }) => {
           onChange={handlePageChange}
         />
       </S.PaginationBox>
-=======
-        <S.TopBox openRoomSDB={openRoomSDB} openSNB={openSNB}>
-          <h1>스포츠에 대한 고찰</h1>
-          <p>100개의 노트</p>
-        </S.TopBox>
-        <RoomNoteBox openRoomSDB={openRoomSDB} openSNB={openSNB} />
-      </S.NoteList>
->>>>>>> main
     </S.Container>
   );
 };
