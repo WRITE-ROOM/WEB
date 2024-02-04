9af4a887e2d75b25b7e00443afbcd41142190e75
youngju6143 <youngju6143@naver.com>
2024-02-04 18:23:43 +0900

    #38feat: 계정설정 URL로 변경

--- writeroom/src/components/Header/MyProfileMenu.jsx
+++ writeroom/src/components/Header/MyProfileMenu.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { DropdownContainer } from "./Dropdown.style";
+import { useNavigate } from "react-router-dom";
 
 const MyProfileMenu = () => {
   return (
     <DropdownContainer>
       <ul>
         <li>
-          <a href="/">계정설정</a>
+          <a href="/myprofile/account">계정설정</a>
         </li>
         <li>
           <a href="/">북마크</a>
