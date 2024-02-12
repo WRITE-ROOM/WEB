30d7c272b7c004655ef45abfe76478a463e095cd
youngju6143 <youngju6143@naver.com>
2024-02-12 19:22:06 +0900

    #70style: 룸초대 모달 스타일 변경

--- writeroom/src/components/Main/InviteModal/InviteModal.jsx
+++ writeroom/src/components/Main/InviteModal/InviteModal.jsx
@@ -35,8 +35,7 @@ return (
 				<h6>룸 초대 URL</h6>
 				<p>{`localhost:3000/room/${roomIdList[roomIndex]}`}</p>
 			</S.RoomName>
-			<h5>※ 이 URL을 알고 있는 사람은 누구나 이 룸에 접속 가능합니다.</h5>
-			<h5>불특정 다수가 볼 가능성이 있는 장소에 올리는 것은 권장하지 않습니다. ※</h5>
+			<h5>※ 이 URL을 알고 있는 사람은 누구나 이 룸에 접속 가능합니다.<br/>불특정 다수가 볼 가능성이 있는 장소에 올리는 것은 권장하지 않습니다. ※</h5>
 			<S.CopyBtn onClick={() => {copyURL(`localhost:3000/room/${roomIdList[roomIndex]}`)}}>
 				<FiCopy color='white'/>
 				<p>링크 복사</p>
