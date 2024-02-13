948e1965b4480b263ab263dd634c92f8b584e785
youngju6143 <youngju6143@naver.com>
2024-02-13 14:45:03 +0900

    #84feat: 룸 생성 모달창 수정

--- writeroom/src/components/Main/MainBox/MainBox.jsx
+++ writeroom/src/components/Main/MainBox/MainBox.jsx
@@ -5,22 +5,15 @@ import RecTopicClose from "../../RecTopicClose/RecTopicClose";
 import MainInfo from "../MainInfo/MainInfo";
 import NewNoteButton from "../../FloatingButton/NewNoteButton";
 import NewRoomButton from "../../FloatingButton/NewRoomButton";
-import NewRoomModal from "../NewRoomModal/NewRoomModal";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { resetRoom, setRoom } from "../../../redux/room";
-import { store } from "../../../redux/store";
 import { selectRoomIds } from "../../../redux/room";
 
 export default function MainBox() {
   const [isSNBOpen, setIsSNBOpen] = useState(false);
-  const [isModalOpen, setIsModalOpen] = useState(false);
   const roomIdList = useSelector(selectRoomIds);
-
-  const user = useSelector((state) => state.user);
-  // const userId = user.userId;
-  // const receivedToken = user.accessToken;
   const rooms = useSelector((state) => state.room.room);
 	
 	let navigate = useNavigate();
@@ -29,13 +22,6 @@ export default function MainBox() {
 		setIsSNBOpen((prev) => !prev);
 	};
 
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
 
 	// const fetchRoomList = async () => {
   //   const userId = localStorage.getItem('id');
@@ -106,8 +92,7 @@ export default function MainBox() {
             </S.Room>
           ))}
         </S.Container>
-        <NewNoteButton /> <NewRoomButton onClick={openModal} />
-        {/* <NewRoomModal isOpen={isModalOpen} onClose={closeModal} /> */}
+        <NewNoteButton /> <NewRoomButton />
         {isSNBOpen ? (
           <RecTopic onToggle={toggleSNB}></RecTopic>
         ) : (
