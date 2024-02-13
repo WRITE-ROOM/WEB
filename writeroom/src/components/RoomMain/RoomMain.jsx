93bdf0f5902982a0fa75e3c9fd2d6762c0898b94
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2024-02-13 16:13:39 +0900

    feature/#85: 룸 메인 api 재연결

--- writeroom/src/components/RoomMain/RoomMain.jsx
+++ writeroom/src/components/RoomMain/RoomMain.jsx
@@ -13,78 +13,35 @@ import axios from "axios";
 const RoomMain = ({ openRoomSNB, openSNB }) => {
   const dispatch = useDispatch();
   const receivedToken = localStorage.getItem("token");
-  const getNoteList = async () => {
-    try {
-      const response = await axios.get("/rooms/13/list?page=0", {
-        headers: {
-          Authorization: `Bearer ${receivedToken}`,
-        },
-      });
-      console.log(response.data);
-      dispatch(resetRoomInfo());
-      dispatch(
-        setRoomInfo({
-          roomId: response.data.roomId,
-          roomTitle: response.data.roomTitle,
-          roomIntroduction: response.data.roomIntroduction,
-          updatedAt: response.data.updatedAt,
-          roomImg: response.data.roomImg,
-          userRoomList: response.data.userRoomList,
-          totalElements: response.data.totalElements,
-          listSize: response.data.listSize,
-          noteList: response.data.noteList,
-        })
-      );
-    } catch (error) {
-      console.error("룸 메인 에러:", error);
-    }
-  };
-
   const roomInfoSelector = useSelector(selectRoomInfoState);
 
   useEffect(() => {
+    const getNoteList = async () => {
+      try {
+        const response = await axios.get("/rooms/13/list?page=0", {
+          headers: {
+            Authorization: `Bearer ${receivedToken}`,
+          },
+        });
+        console.log(response.data.result.roomId);
+        console.log(response.data.result.roomTitle);
+        dispatch(resetRoomInfo());
+        dispatch(setRoomInfo(response.data.result));
+        console.log("roomInfoSelector inside useEffect: ", roomInfoSelector);
+      } catch (error) {
+        console.error("룸 메인 에러:", error);
+      }
+    };
+
     getNoteList();
   }, []);
+
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 10;
   const totalItems = roomInfoSelector?.listSize;
   const handlePageChange = (page) => {
     setCurrentPage(page);
   };
-
-
-  // console.log(roomSelector.room);
-  const currentItems = roomInfoSelector?.noteList;
-  const accessToken = localStorage.getItem("token");
-  // const accessToken =
-  // "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjksImVtYWlsIjoidGVzdFVzZXJAbmF2ZXIuY29tIiwicm9sZSI6IlVTRVIiLCJpYXQiOjE3MDcxNTEwNDQsImV4cCI6MTc5MzU1MTA0NH0.Dsm7MWG8y-zUQnhRTe5P0ndFCjbhVU1z8mYwj1hqASo";
-
-  const { roomId } = useParams();
-
-  const fetchNoteList = async () => {
-    try {
-      const params = { page: 0 };
-      const res = await axios.get(`/rooms/${roomId}/list`, {
-        params,
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      });
-      resetNote();
-      setRoomTitle(res.data.result.roomTitle);
-      setRoomIntro(res.data.result.roomIntroduction);
-      dispatch(setNoteList(res.data.result.noteList));
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const noteList = useSelector((state) => state.noteList);
-
-  useEffect(() => {
-    fetchNoteList();
-  }, []);
-  console.log("noteLIST", noteList);
 
   return (
     <S.Container>
@@ -102,13 +59,13 @@ const RoomMain = ({ openRoomSNB, openSNB }) => {
             <TagSearchBox />
             <p>{roomInfoSelector.totalElements}개의 노트</p>
           </S.TopBox>
-          {currentItems &&
-            currentItems.map((note, index) => (
+          {roomInfoSelector.noteList &&
+            roomInfoSelector.noteList.map((note, index) => (
               <ImageRoomNoteBox
                 key={index}
                 openRoomSNB={openRoomSNB}
                 openSNB={openSNB}
-                note={note} // 각 노트에 대한 데이터 전달
+                note={note}
               />
             ))}
         </S.NoteList>
--- writeroom/src/redux/store.jsx
+++ writeroom/src/redux/store.jsx
@@ -5,10 +5,10 @@ import tagSlice from "./tag";
 import selectModalSlice from "./selectModal";
 import categorySlice from "./category";
 import noteSlice from "./note";
-import {bookmark} from './bookmark';
+import { bookmark } from "./bookmark";
 import noteListSlice from "./noteList";
 import writeModeSlice from "./writeMode";
-
+import roomInfoSlice from "./roomInfo";
 export const store = configureStore({
   reducer: {
     user: user.reducer,
@@ -20,5 +20,6 @@ export const store = configureStore({
     bookmark: bookmark.reducer,
     noteList: noteListSlice,
     writeMode: writeModeSlice,
+    roomInfo: roomInfoSlice,
   },
 });
