15b55264ed9794db50b723a9fda1ecfb3ea97b89
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2024-02-18 18:16:41 +0900

    오류 수정

---
+++ writeroom/src/hooks/useDebounce.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+const useDebounce = (value, delay = 1000) => {
+  const [debounceVal, setDebounceVal] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebounceVal(value);
+    }, delay);
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+  return debounceVal;
+};
+
+export default useDebounce;
